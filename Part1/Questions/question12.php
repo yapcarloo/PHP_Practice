912b160c5949a0710a6f7327901386cd21406e8d
Jean Carlo Yap <yapjeancarlo@gmail.com>
2025-02-19 22:27:49 +0800

    q12 commit

---
+++ Part1/Questions/question12.php
@@ -0,0 +1,14 @@
+<?php
+// Question Output
+echo "Question 12: What is the output of echo \$matrix[1][2];?<br><br>";
+
+// Define the $matrix as a 2D array
+$matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+// Output the value at row 1, column 2
+echo "The value at \$matrix[1][2] is: " . $matrix[1][2];
+?>
