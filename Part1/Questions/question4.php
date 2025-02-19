770f8f43d6edab312306a95faf6a5cb4041c7cc1
Jean Carlo Yap <yapjeancarlo@gmail.com>
2025-02-19 22:18:05 +0800

    q4 commit

---
+++ Part1/Questions/question4.php
@@ -0,0 +1,10 @@
+<?php
+// Question Output
+echo "Question 4: What character is used to concatenate strings in PHP?<br><br>";
+
+// Example of concatenation
+$firstName = "Jean Carlo";
+$lastName = "Yap";
+$fullName = $firstName . " " . $lastName; // Concatenate with a space in between
+echo "Full name: " . $fullName;
+?>
