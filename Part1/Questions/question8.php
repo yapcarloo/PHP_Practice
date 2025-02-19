125e06e069689a016f1b0e6c0fbd6a9d09f81aad
Jean Carlo Yap <yapjeancarlo@gmail.com>
2025-02-19 22:24:45 +0800

    q8 commit

---
+++ Part1/Questions/question8.php
@@ -0,0 +1,10 @@
+<?php
+// Question Output
+echo "Question 8: What is the output of echo \$colors[3];?<br><br>";
+
+
+$colors = ["Red", "Green", "Blue", "Yellow", "Purple"];
+
+
+echo "The value of \$colors[3] is: " . $colors[3];
+?>
