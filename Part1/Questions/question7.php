21f0f00ecc5f36be7fa973056b07a9dec7bf41f4
Jean Carlo Yap <yapjeancarlo@gmail.com>
2025-02-19 22:24:15 +0800

    q7 commit

---
+++ Part1/Questions/question7.php
@@ -0,0 +1,10 @@
+<?php
+// Question Output
+echo "Question 7: What is the output of echo \$numbers[3] + \$numbers[4];?<br><br>";
+
+// Define the $numbers array with some sample values
+$numbers = [11, 22, 33, 44, 55];
+
+// Perform the operation and output the result
+echo "The result of \$numbers[3] + \$numbers[4] is: " . ($numbers[3] + $numbers[4]);
+?>
