347fb61760ddeabac5264c24f16a600dfc11e693
Jean Carlo Yap <yapjeancarlo@gmail.com>
2025-02-19 22:15:53 +0800

    q1 commit

---
+++ Part1/Questions/question1.php
@@ -0,0 +1,15 @@
+<?php
+// Question Output
+echo "Question 1: What is the output of print_r(\$person);?<br><br>";
+
+// Define the $person array
+$person = [
+    'name' => 'Jean Carlo Yap',
+    'age' => 24,
+    'email' => 'yapjeancarlo@gmail.com'
+];
+
+// Use print_r to print the array
+echo "The output of print_r(\$person):<br>";
+print_r($person);
+?>
