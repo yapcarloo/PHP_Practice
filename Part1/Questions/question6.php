f07275037cb34f939dd02f0578b44a9b19029346
Jean Carlo Yap <yapjeancarlo@gmail.com>
2025-02-19 22:23:30 +0800

    q6 commit

---
+++ Part1/Questions/question6.php
@@ -0,0 +1,10 @@
+<?php
+// Question Output
+echo "Question 6: How will you define in PHP the variable PI with value 3.1416?<br><br>";
+
+// Define the constant PI with value 3.1416
+define("PI", 3.1416);
+
+// Output the value of PI
+echo "The value of PI is: " . PI;
+?>
