396b92d561dd26e23ed9dcfb0a695ac5c397be42
Jean Carlo Yap <yapjeancarlo@gmail.com>
2025-02-19 22:17:17 +0800

    q3 commit

---
+++ Part1/Questions/question3.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Jean Carlo Yap";
+$age = 24;
+
+// Concatenate variables with strings using the dot operator
+echo "My name is " . $name . " and I am " . $age . " years old.";
+?>
+<!
